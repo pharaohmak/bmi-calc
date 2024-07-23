25a9980f97c947725e63d12670d0183a26846949
Makram M. Ibrahim <61184060+pharaohmak@users.noreply.github.com>
2024-07-23 14:12:23 -0700

    refactored

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// [weight (lb) / height (in) / height (in)] x 703
-
 document.querySelector("#bmi-btn").addEventListener("click", function (e) {
     // Validation!
     if (document.querySelector("#feet").reportValidity() &&
         document.querySelector("#inches").reportValidity() &&
         document.querySelector("#weight").reportValidity()) {
         // Get values from form
-        let feet = document.querySelector("#feet").value;
+        let feet = parseFloat(document.querySelector("#feet").value);
         let inches = parseFloat(document.querySelector("#inches").value);
         let weight = parseFloat(document.querySelector("#weight").value);
 
@@ -26,20 +24,16 @@ document.querySelector("#bmi-btn").addEventListener("click", function (e) {
             document.querySelector("#img-result").src = "./assets/under-weight.png";
             document.querySelector("#img-result").style.display = "block";
 
-            // Show #finalResultsContainer for output (display: block)
-            document.querySelector("#result").style.display = "block";
-        }
-        else if (BMI >= 18.5 && BMI <= 24.9) {
+        } else if (BMI >= 18.5 && BMI <= 24.9) {
             // Show text result
             document.querySelector("#result").innerHTML = `Your BMI is ${BMI}, indicating your weight is in the Normal or Healthy Weight Range.`;
 
             // Show image result
             document.querySelector("#img-result").alt = "Normal or Healthy Weight Range";
             document.querySelector("#img-result").src = "./assets/healthy-weight.png";
-            document.querySelector("#img-result").style.display = "flex";
+            document.querySelector("#img-result").style.display = "block";
 
-        }
-        else if (BMI >= 25.0 && BMI <= 29.9) {
+        } else if (BMI >= 25.0 && BMI <= 29.9) {
             // Show text result
             document.querySelector("#result").innerHTML = `Your BMI is ${BMI}, indicating your weight is in the Over Weight Range.`;
 
@@ -48,8 +42,7 @@ document.querySelector("#bmi-btn").addEventListener("click", function (e) {
             document.querySelector("#img-result").src = "./assets/over-weight.png";
             document.querySelector("#img-result").style.display = "block";
 
-        }
-        else if (BMI >= 30.0 && BMI <= 34.9) {
+        } else if (BMI >= 30.0 && BMI <= 34.9) {
             // Show text result
             document.querySelector("#result").innerHTML = `Your BMI is ${BMI}, indicating your weight is in the Obese Range.`;
 
@@ -58,8 +51,7 @@ document.querySelector("#bmi-btn").addEventListener("click", function (e) {
             document.querySelector("#img-result").src = "./assets/obese-weight.png";
             document.querySelector("#img-result").style.display = "block";
 
-        }
-        else if (BMI >= 35) {
+        } else if (BMI >= 35) {
             // Show text result
             document.querySelector("#result").innerHTML = `Your BMI is ${BMI}, indicating your weight is in the Extreme Obese Range.`;
 
@@ -68,8 +60,7 @@ document.querySelector("#bmi-btn").addEventListener("click", function (e) {
             document.querySelector("#img-result").src = "./assets/extremly-obese.png";
             document.querySelector("#img-result").style.display = "block";
 
-        }
-        else if ((feet == 0 && inches == 0) || weight == 0) {
+        } else if ((feet == 0 && inches == 0) || weight == 0) {
             // Show text result
             document.querySelector("#result").innerHTML = `Please check the accuracy of the information you entered:<br><br>Height: ${feet} feet, ${inches} inches<br><br>Weight: ${weight} pounds`;
 
